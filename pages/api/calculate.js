d0fa329d7c867b08c96a9cb28ed08137493edfab
Tan Doan <tdoan87@gmail.com>
2021-01-10 14:12:47 -0700

    Get relevant transactions

--- pages/api/calculate.js
+++ pages/api/calculate.js
@@ -1,12 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default async (req, res) => {
-  const start = parseInt((new Date() - 5 * 60 * 1000) / 1000)
+  const start = parseInt((new Date() - 60 * 60 * 1000) / 1000);
 
   const data = await fetch(
     `https://api.whale-alert.io/v1/transactions?api_key=${process.env.WHALE_ALERT_API_KEY}&min_value=10000000&start=${start}`
   ).then(res => res.json())
 
-  res.statusCode = 200
-  res.json(data)
+  const transactions = data.transactions;
+
+  const relevantTransactions = transactions
+    .filter(t => {
+      return (
+        t.from.owner_type !== 'unknown' && t.to.owner_type === 'unknown' ||
+        t.from.owner_type === 'unknown' && t.to.owner_type !== 'unknown'
+      )
+    })
+
+  res.statusCode = 200;
+  res.json({ transactions: relevantTransactions });
 }
